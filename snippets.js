94e7117836cc3e1a8cfd39e09b104f323fce703c
AldairGonzalezA <aldairalejandrogonzalezaraujo@gmail.com>
2025-01-22 17:10:38 -0600

    Trabajo en Clase 22/01

--- snippets.js
+++ snippets.js
@@ -1,11 +1,27 @@
-document.getElementById('from').addEventListener('submit', function(event){
-    event.preventDefault();
+document.addEventListener('DOMContentLoaded', function() {
+    const formulario = document.getElementById('form');
+    if (formulario) {
+        formulario.addEventListener('submit', function(event) {
+            event.preventDefault();
 
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+            const username = document.getElementById('email').value;
+            const password = document.getElementById('password').value;
+            const usuario = {
+                correo: username,
+                contra: password
+            };
+            console.log(usuario);
+            localStorage.setItem('Usuario', JSON.stringify(usuario));
 
-    localStorage.setItem('username',username);
-    localStorage.setItem('password',password);
+            window.location.href = 'Contactos.html';
+        });
+    } else {
+        console.error("Formulario no encontrado");
+    }
 
-    window.location.href = '';
+    const user = JSON.parse(localStorage.getItem('Usuario'));
+    if (user) {
+        document.getElementById('emailLabel').innerHTML = `${user.correo}`;
+        document.getElementById('passwordLabel').innerHTML = `${user.contra}`;
+    }
 });
